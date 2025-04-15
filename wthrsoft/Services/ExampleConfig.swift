615d7d6eccebc1f1bea1af43814520b2b582e0d2
sgazz <sgazza@outlook.com>
2025-04-15 08:44:09 +0200

    Ispravljen konflikt imena strukture u ExampleConfig.swift

--- wthrsoft/Services/ExampleConfig.swift
+++ wthrsoft/Services/ExampleConfig.swift
@@ -2,7 +2,7 @@ import Foundation
 
 // Primer konfiguracionog fajla
 // Kopirajte ovaj fajl kao Config.swift i unesite svoje API ključeve
-struct Config {
+struct ExampleConfig {
     static let apiKey = "YOUR_OPENWEATHER_API_KEY"  // OpenWeather API key
     static let astronomyApiKey = "YOUR_WEATHERAPI_KEY"  // WeatherAPI key for astronomy data
 } 
