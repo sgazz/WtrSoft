5dde75ab32436e346d9d6de24d14691b587909e7
sgazz <sgazza@outlook.com>
2025-04-15 08:34:22 +0200

    Ažuriran ExampleConfig.swift sa strukturom za oba API ključa

--- wthrsoft/Services/ExampleConfig.swift
+++ wthrsoft/Services/ExampleConfig.swift
@@ -1,7 +1,8 @@
 import Foundation
 
 // Primer konfiguracionog fajla
-// Kopirajte ovaj fajl kao Config.swift i unesite svoj API ključ
-struct ExampleConfig {
-    static let apiKey = "YOUR_API_KEY_HERE"
+// Kopirajte ovaj fajl kao Config.swift i unesite svoje API ključeve
+struct Config {
+    static let apiKey = "YOUR_OPENWEATHER_API_KEY"  // OpenWeather API key
+    static let astronomyApiKey = "YOUR_WEATHERAPI_KEY"  // WeatherAPI key for astronomy data
 } 
